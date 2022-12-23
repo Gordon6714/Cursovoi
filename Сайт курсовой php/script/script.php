901650a6105ef408435968246b51882f7400708f
Gordon6714 <93514043+Gordon6714@users.noreply.github.com>
2022-12-23 13:57:42 +0500

    Add files via upload

--- Сайт курсовой php/script/script.php
+++ Сайт курсовой php/script/script.php
@@ -12,11 +12,32 @@ if($link == false){
 } else{
     print("Успешно </br>");
 
-    /*$sql = 'INSERT INTO bilet SET id_filma = "'.$name_film_int.'", id_adress = "'.$adress_kino_int.'", id_mesto = "'.$mesto_int.'"';
-    $result = mysqli_query($link,$sql); 
+    $query = "SELECT id_filma FROM `film` WHERE nazvanie = '$name_film'";
+    $result_select = mysqli_query($link, $query);
+
+    while ($object = mysqli_fetch_array($result_select)){
+        $key_film = $object['id_filma'];
+    }
+
+    $query = "SELECT id_adressa FROM `adress` WHERE adress = '$adress_kino'";
+    $result_select = mysqli_query($link, $query);
+
+    while ($object = mysqli_fetch_array($result_select)){
+        $key_adressa = $object['id_adressa'];
+    }
+
+    $query = "SELECT id_mesta FROM `mesto` WHERE mesto = '$mesto'";
+    $result_select = mysqli_query($link, $query);
+
+    while ($object = mysqli_fetch_array($result_select)){
+        $key_mesta = $object['id_mesta'];
+    }
+
+    $sql = "INSERT INTO bilet (id_filma , id_adress , id_mesto) VALUE ( '$key_film' , '$key_adressa' , '$key_mesta' )";
+    $result = mysqli_query($link,$sql);
     if ($result == false){
         print("Ошибка при выполнении запроса фильма");
-    }*/
+    }
 
 
 
